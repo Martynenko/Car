d4de186e09c72628ad63e7f4eeecdd052d71f057
Мартыненко Артем Сергеевич <artem.martynenko@privatbank.ua>
2020-07-09 17:39:46 +0300

    release v4

--- Main.java
+++ Main.java
@@ -1,21 +1,21 @@
 public class Main {
 
     public static void main(String[] args) {
-      Car car = new Car();
+      /*Car car = new Car();
         car.type = "crossover";
         car.averageSpeed = 100;
         car.quantityOfDoors = 4;
 
 
         System.out.println("модель авто - " + car.type);
-        System.out.println("скорость авто - " + car.averageSpeed);
+        System.out.println("скорость авто - " + car.averageSpeed);*/
 
-        Car bmv = new Car();
+        BMV bmv = new BMV();
         bmv.setType("bmv");
         bmv.run();
         bmv.stop();
 
-        Car mazda = new Car();
+        Mazda mazda = new Mazda();
         mazda.setType("mazda");
         mazda.run();
         mazda.stop();
